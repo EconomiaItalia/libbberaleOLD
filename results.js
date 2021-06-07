12663058299cc0c73019c766493a1e4a0bb37210
EconomiaItalia <82286837+EconomiaItalia@users.noreply.github.com>
2021-06-07 18:19:28 +0200

    Update results.js

--- results.js
+++ results.js
@@ -2,7 +2,7 @@ results = [
   {"name": "Dino Parrano", "image": "./img/Dino\ Parrano.svg", "x_axis": 3.38, "y_axis": -5.13},
   {"name": "Costantino De Blasi", "image": "./img/Costantino\ De\ Blasi.svg", "x_axis": 5.67, "y_axis": -1.15},
   {"name": "Entropy for Life", "image": "./img/Entropy\ for\ life.svg", "x_axis": 3, "y_axis": -1.54},
-  {"name": "Lucio Di Gaetano", "image": "./img/Lucio\ Di\ Gaetano.svg", "x_axis": 7.33, "y_axis": -6.54},
+  {"name": "Lucio Di Gaetano", "image": "./img/Lucio Di Gaetano.svg", "x_axis": 7.33, "y_axis": -6.54},
   {"name": "Onorevole berti", "image": "./img/on Berti.svg", "x_axis": 3, "y_axis": -1.92},
   {"name": "Marco Cartasegna", "image": "./img/Marco\ Cartasegna.svg", "x_axis": 5, "y_axis": 0.38},
   {"name": "C'è di peggio", "image": "./img/C\'è\ di\ peggio.svg", "x_axis": 6.33, "y_axis": -2.31},
