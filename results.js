e5f73d59de2a892910d5717fdfc243380e080343
EconomiaItalia <82286837+EconomiaItalia@users.noreply.github.com>
2021-06-07 18:05:17 +0200

    Create results.js

---
+++ results.js
@@ -0,0 +1,11 @@
+results = {
+  {"Dino Parrano", "img/Dino Parrano.svg", 3.38, -5.13},
+  {"Costantino De Blasi", "img/Costantino De Blasi.svg", 5.67, -1.15},
+  {"Entropy for Life", "img/Entropy for life.svg", 3, -1.54},
+  {"Lucio Di Gaetano", "img/Lucio Di Gaetano.svg", 7.33, -6.54},
+  {"Onorevole berti", "img/on Berti.svg", 3, -1.92},
+  {"Marco Cartasegna", "img/Marco Cartasegna.svg", 5, 0.38},
+  {"C'è di peggio", "img/C'è di peggio.svg", 6.33, -2.31},
+  {"Gaia e Pietro", "img/Gaia Zorzi.svg", 4, -2.69},
+  {"Rick du Fer", "img/Rick DuFer.svg", 5.33, -3.85}
+};
