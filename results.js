4c6f9abeb4f2413651d86d43585d1d52f8421062
EconomiaItalia <82286837+EconomiaItalia@users.noreply.github.com>
2021-06-07 18:16:07 +0200

    Update results.js

--- results.js
+++ results.js
@@ -1,11 +1,11 @@
-results = {
-  {"Dino Parrano", "img/Dino Parrano.svg", 3.38, -5.13},
-  {"Costantino De Blasi", "img/Costantino De Blasi.svg", 5.67, -1.15},
-  {"Entropy for Life", "img/Entropy for life.svg", 3, -1.54},
-  {"Lucio Di Gaetano", "img/Lucio Di Gaetano.svg", 7.33, -6.54},
-  {"Onorevole berti", "img/on Berti.svg", 3, -1.92},
-  {"Marco Cartasegna", "img/Marco Cartasegna.svg", 5, 0.38},
-  {"C'è di peggio", "img/C'è di peggio.svg", 6.33, -2.31},
-  {"Gaia e Pietro", "img/Gaia Zorzi.svg", 4, -2.69},
-  {"Rick du Fer", "img/Rick DuFer.svg", 5.33, -3.85}
-};
+results = [
+  {"name": "Dino Parrano", "image": "img/Dino Parrano.svg", "x_axis": 3.38, "y_axis": -5.13},
+  {"name": "Costantino De Blasi", "image": "img/Costantino De Blasi.svg", "x_axis": 5.67, "y_axis": -1.15},
+  {"name": "Entropy for Life", "image": "img/Entropy for life.svg", "x_axis": 3, "y_axis": -1.54},
+  {"name": "Lucio Di Gaetano", "image": "img/Lucio Di Gaetano.svg", "x_axis": 7.33, "y_axis": -6.54},
+  {"name": "Onorevole berti", "image": "img/on Berti.svg", "x_axis": 3, "y_axis": -1.92},
+  {"name": "Marco Cartasegna", "image": "img/Marco Cartasegna.svg", "x_axis": 5, "y_axis": 0.38},
+  {"name": "C'è di peggio", "image": "img/C'è di peggio.svg", "x_axis": 6.33, "y_axis": -2.31},
+  {"name": "Gaia e Pietro", "image": "img/Gaia Zorzi.svg", "x_axis": 4, "y_axis": -2.69},
+  {"name": "Rick du Fer", "image": "img/Rick DuFer.svg", "x_axis": 5.33, "y_axis": -3.85}
+];
