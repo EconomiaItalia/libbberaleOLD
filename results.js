f74987bf8e539de4a27426214344db0a6e5ce539
EconomiaItalia <82286837+EconomiaItalia@users.noreply.github.com>
2021-06-28 17:18:00 +0200

    Update results.js

--- results.js
+++ results.js
@@ -7,5 +7,8 @@ results = [
   {"name": "Marco Cartasegna", "image": "./img/Marco Cartasegna.svg", "x_axis": 5, "y_axis": 0.38},
   {"name": "C'è di peggio", "image": "./img/C'è di peggio.svg", "x_axis": 6.33, "y_axis": -2.31},
   {"name": "Gaia e Pietro", "image": "./img/Gaia Zorzi.svg", "x_axis": 4, "y_axis": -2.69},
-  {"name": "Rick du Fer", "image": "./img/Rick DuFer.svg", "x_axis": 5.33, "y_axis": -3.85}
+  {"name": "Rick du Fer", "image": "./img/Rick DuFer.svg", "x_axis": 5.33, "y_axis": -3.85},
+  {"name": "Boban Pesov", "image": "./img/Bobanpesov.svg", "x_axis": 1.25, "y_axis": -1.67},
+  {"name": "Andrea Lorenzon", "image": "./img/Lorenzon.svg", "x_axis": -1.33, "y_axis": 2.69},
+  {"name": "Carlo Stagnaro", "image": "./img/Carlo Stagnaro.svg", "x_axis": 5.83, "y_axis": -6.67}
 ];
